a38181ae6c721a7e74e63179ce2b6fa556e9343b
Tony2100 <tony1381471821@gmail.com>
2022-11-23 21:24:58 +0800

    Remove debug code

--- main.py
+++ main.py
@@ -7,10 +7,6 @@ app = Flask(__name__, static_url_path='/static')
 path = './Uploads'
 exclude = ['Thumbs.db', '.DS_Store']
 
-@app.route('/test')
-def test():
-    return render_template('test.html')
-
 @app.route('/')
 def index():
     all_files = []
@@ -24,22 +20,23 @@ def index():
                 file_data.append(datetime.fromtimestamp(
                     os.path.getmtime(fullpath)).strftime('%Y-%m-%d %H:%M:%S'))
                 size_bytes = os.path.getsize(fullpath)
-                file_data.append(round(size_bytes / 1000000,3).__str__() + ' MB')
+                file_data.append(
+                    round(size_bytes / 1000000, 3).__str__() + ' MB')
                 all_files.append(file_data)
     all_files.sort(key=lambda x: x[1])
     all_files.reverse()
     return render_template('index.html', **locals())
 
+
 @app.route('/upload', methods=['POST'])
 def upload_file():
     if request.method == 'POST':
         if 'file' not in request.files:
-            print('No file part')
-            return ('No file part')
+            return 'No file part'
         file = request.files['file']
         print(file)
         if file.filename == '':
-            return ('No selected file')
+            return 'No selected file'
         if file:
             while file.filename in os.listdir(path):
                 file.filename = '-' + file.filename
@@ -51,6 +48,7 @@ def upload_file():
 def download_file(name):
     return send_from_directory(path, name)
 
+
 @app.route('/delfile/<string:file>')
 def del_file(file):
     try:
@@ -58,6 +56,7 @@ def del_file(file):
         return "OK"
     except FileNotFoundError:
         return "Not Found"
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5090)
